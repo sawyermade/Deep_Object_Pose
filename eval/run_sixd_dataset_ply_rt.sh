4802077273ade5277fa1ae5b4adb3d681466a446
Daniel Sawyer <sawyermade0@gmail.com>
2019-08-15 13:55:16 -0400

    lazy

--- eval/run_sixd_dataset_ply_rt.sh
+++ eval/run_sixd_dataset_ply_rt.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-python3 -u sixd_dataset_ply_rt.py ~/DATASETS/linemod_plus/ -v | tee sixd_dataset_ply_rt.output
+python3 -u sixd_dataset_ply_rt.py ~/DATASETS/linemod_plus | tee sixd_dataset_ply_rt.output

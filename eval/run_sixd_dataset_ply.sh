a26723a20945f9b84f88d3bd16525cd3937636a1
Daniel Sawyer <sawyermade0@gmail.com>
2019-08-20 14:11:34 -0400

    moved_dirs

--- eval/run_sixd_dataset_ply.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-python3 -u sixd_dataset_ply.py ~/DATASETS/linemod_plus/ -v | tee sixd_dataset_ply.output
--- eval/run_sixd_dataset_ply_rt.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-python3 -u sixd_dataset_ply_rt.py ~/DATASETS/linemod_plus | tee sixd_dataset_ply_rt.output

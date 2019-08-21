081c5b7fab58a991cbb4b906275dabe937c413b7
Daniel Sawyer <sawyermade0@gmail.com>
2019-08-21 03:39:50 -0400

    lazy

--- eval/sixd_dataset_transform/run_sixd_dataset_ply.sh
+++ eval/sixd_dataset_transform/run_sixd_dataset_ply.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-python3 -u sixd_dataset_ply.py ~/DATASETS/linemod_plus/ -v | tee sixd_dataset_ply.output
+# python3 -u sixd_dataset_ply.py ~/DATASETS/linemod_plus/ -v | tee sixd_dataset_ply.output
+python3 sixd_dataset_ply.py ~/DATASETS/linemod_plus/
--- eval/sixd_dataset_transform/run_sixd_dataset_ply_rt.sh
+++ eval/sixd_dataset_transform/run_sixd_dataset_ply_rt.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-python3 -u sixd_dataset_ply_rt.py ~/DATASETS/linemod_plus | tee sixd_dataset_ply_rt.output
+# python3 -u sixd_dataset_ply_rt.py ~/DATASETS/linemod_plus | tee sixd_dataset_ply_rt.output
+python3 sixd_dataset_ply_rt.py ~/DATASETS/linemod_plus

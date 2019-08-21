a71a1669aed0c00dcccc78943954cdc523c82b14
Daniel Sawyer <sawyermade0@gmail.com>
2019-08-21 03:02:19 -0400

    lazy

--- eval/sixd_dataset_transform/sixd_dataset_keypoints.py
+++ eval/sixd_dataset_transform/sixd_dataset_keypoints.py
@@ -347,10 +347,10 @@ def main():
 
 	# Completed
 	time_stop = time.time()
-	time_total = time_stop - time_start
-	time_minutes = time_total // 60
-	time_hours = time_total // 60**2
-	time_seconds = time_total % 60
+	time_total = int(time_stop - time_start)
+	time_minutes = int(time_total // 60)
+	time_hours = int(time_total // 60**2)
+	time_seconds = int(time_total % 60)
 	print(f'Conversions Completed: {str(time_hours).zfill(2)}:{str(time_minutes).zfill(2)}:{str(time_seconds).zfill(2)}')
 
 if __name__ == '__main__':
